885fc1dd356a5a1fefcfab5caa94bd00d4097e2d
dolfinus <martinov_m_s_@mail.ru>
2018-10-17 23:54:31 +0300

    Make ActiveRecord class abstract

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def initialize(params = {})
+    super(params)
+    params.each { |key, value| send "#{key}=", value } unless params.nil?
+  end
 end
