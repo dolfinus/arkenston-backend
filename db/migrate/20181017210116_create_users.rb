2014e0c0f5b8a0946c8aba77c0c37764c85e6f25
dolfinus <martinov_m_s_@mail.ru>
2018-10-18 00:02:15 +0300

    Create Users db migration

---
+++ db/migrate/20181017210116_create_users.rb
@@ -0,0 +1,32 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string  :name
+      t.string  :first_name
+      t.string  :middle_name
+      t.string  :last_name
+      t.string  :email,              null: false, default: ""
+      t.string  :encrypted_password, null: false, default: ""
+      t.string  :remember_token,     null: false, default: ""
+      t.string  :confirmation_token
+      t.integer :role
+      t.timestamps
+    end
+
+    reversible do |dir|
+      dir.up do
+        User.create_translation_table! first_name: :string, middle_name: :string, last_name: :string
+      end
+
+      dir.down do
+        User.drop_translation_table!
+      end
+    end
+
+    add_index :users, :name,               unique: true
+    add_index :users, :email,              unique: true
+    add_index :users, :confirmation_token, unique: true
+    add_index :users, :remember_token,     unique: true
+    add_index :users, [:id, :name, :role]
+  end
+end
