0c141c6ed8b56a18305dab4cec00c67cf8b9b13e
dolfinus <martinov_m_s_@mail.ru>
2018-10-18 00:00:32 +0300

    Add User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
   include Clearance::User
+  has_paper_trail
+
+  validates_format_of :name, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
+  translates :first_name, :middle_name, :last_name, fallbacks_for_empty_translations: true, versioning: :paper_trail
+
+  def to_param
+    name
+  end
+
+  def full_name
+    [first_name, middle_name, last_name].join ' '
+  end
 end
