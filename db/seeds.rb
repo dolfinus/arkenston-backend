c09484396f496e509d2cdf7a4c9c9d698c982111
dolfinus <martinov_m_s_@mail.ru>
2019-01-03 14:16:28 +0300

    Add anonymous user to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,27 @@
 def seed
+  add_anonymous
   add_admin
+end
+
+def add_anonymous
+  User.connection.schema_cache.clear!
+  User.reset_column_information
+
+  existing_anonymous = User.find_by(name: 'anonymous')
+
+  unless existing_anonymous.present?
+    new_anonymous = User.new(
+      name:           'anonymous',
+      role:           :user,
+      first_name:     '',
+      middle_name:    '',
+      last_name:      '',
+      email:          '',
+      password:       '',
+      remember_token: ''
+    )
+    new_anonymous.save(validate: false)
+  end
 end
 
 def add_admin
