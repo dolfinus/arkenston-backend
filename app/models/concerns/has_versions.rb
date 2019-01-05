f0b6786b469f88ce4637c2b82f2a3ecf99336226
dolfinus <martinov_m_s_@mail.ru>
2019-01-05 21:40:06 +0300

    Use ActiveSupport::Concern for HasVersions

--- app/models/concerns/has_versions.rb
+++ app/models/concerns/has_versions.rb
@@ -1,36 +1,38 @@
 module HasVersions
-  def self.included(base)
-    base.class_eval do
-      def self.versioning_class(base_class = self)
-        base_class.version_class.class_name if base_class.respond_to?(:version_class)
-        'EntityVersion'
-      end
+  extend ActiveSupport::Concern
 
-      def self.translation_versioning_class(base_class)
-        versioning_class(base_class.translation_class) if base_class.respond_to?(:translation_class)
-        versioning_class(base_class)
-      end
+  included do
+    has_paper_trail(
+      skip: skipped_versioning_attrs,
+      class_name: versioning_class
+    )
+  end
 
-      def self.skipped_versioning_attrs(base_class = self)
-        [] unless base_class.respond_to?(:skipped_version_attrs)
-        base_class.skipped_version_attrs
-      end
+  module ClassMethods
+    def versioning_class(base_class = self)
+      base_class.version_class.class_name if base_class.respond_to?(:version_class)
+      'EntityVersion'
+    end
 
-      def self.translation_versioning_options
-        {
-          versioning: {
-            gem: :paper_trail,
-            options: {
-              class_name: translation_versioning_class(self)
-            }
+    def translation_versioning_class(base_class)
+      versioning_class(base_class.translation_class) if base_class.respond_to?(:translation_class)
+      versioning_class(base_class)
+    end
+
+    def skipped_versioning_attrs(base_class = self)
+      [] unless base_class.respond_to?(:skipped_version_attrs)
+      base_class.skipped_version_attrs
+    end
+
+    def translation_versioning_options
+      {
+        versioning: {
+          gem: :paper_trail,
+          options: {
+            class_name: translation_versioning_class(self)
           }
         }
-      end
-
-      has_paper_trail(
-        skip: base.skipped_versioning_attrs,
-        class_name: base.versioning_class
-      )
+      }
     end
   end
 end
