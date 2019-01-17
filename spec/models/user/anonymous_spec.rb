5d6c22c1f272c21525dc20b2ac40a74c68791790
dolfinus <martinov_m_s_@mail.ru>
2019-01-18 01:48:42 +0300

    Add Auth method tests

--- spec/models/user/anonymous_spec.rb
+++ spec/models/user/anonymous_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe User, type: :model do
       it 'when Anonymous is exist' do
         expect(User.anonymous).not_to be_nil
       end
+      it 'when Anonymous id is -1' do
+        expect(User.anonymous_id).to eq(-1)
+      end
       it 'when Anonymous name is "anonymous"' do
         expect(User.anonymous_name).to eq('anonymous')
       end
-    end
-
-    context 'with error' do
       it 'when Anonymous has no email and password' do
         expect(User.anonymous).not_to be_valid
       end
