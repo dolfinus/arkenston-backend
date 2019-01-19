fa36f5584cd333e9377762d34398268bf8ed255f
dolfinus <martinov_m_s_@mail.ru>
2019-01-20 01:20:57 +0300

    Fix missing role_id from HasRole concern

--- app/models/concerns/has_role.rb
+++ app/models/concerns/has_role.rb
@@ -5,6 +5,10 @@ module HasRole
     assignable_values_for :role, through: proc { find_policy(self) }, default: default_role
   end
 
+  def role_id
+    User.all_roles.index(role)
+  end
+
   module ClassMethods
     def all_roles
       %w[user moderator admin]
