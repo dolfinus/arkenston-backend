600be100fe20501f5c0e2016f6d35e46538214e4
dolfinus <martinov_m_s_@mail.ru>
2019-01-27 14:22:10 +0300

    Build Docker images with Travis

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker build -t dolfinus/arkenston-backend:base  -t dolfinus/arkenston-backend:base-latest  --compress -f Dockerfile.base  .
-docker build -t dolfinus/arkenston-backend:build -t dolfinus/arkenston-backend:build-latest --compress -f Dockerfile.build .
-docker-compose -f docker-compose.dev.yml up -d --build
-docker-compose -f docker-compose.test.yml up -d --build
-docker-compose -f docker-compose.yml up -d --build
