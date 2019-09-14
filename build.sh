56755cb2903e55953235e5008cae2ea28ad426b9
dolfinus <martinov_m_s_@mail.ru>
2019-09-14 17:18:32 +0300

    Improve Dockerfile structure

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ENV="$1"
+shift
+COMMAND="$@"
+
+if [[ "x$ENV" == "x" ]]
+then
+  ENV="prod"
+fi
+
+if [[ "x$COMMAND" == "x" ]]
+then
+  COMMAND="up -d --build"
+fi
+
+docker build -t dolfinus/arkenston-backend:base  -f Dockerfile.base .
+docker build -t dolfinus/arkenston-backend:build -f Dockerfile.build .
+docker-compose -f "docker-compose.$ENV.yml" $COMMAND
