cd5b19f5290fefab052e8911455a4466e562af9d
dolfinus <martinov_m_s_@mail.ru>
2020-11-15 21:52:39 +0300

    Allow to pass -h or --help arguments to test container

--- entrypoint.test.sh
+++ entrypoint.test.sh
@@ -4,8 +4,20 @@ set -e
 
 ./wait_for_postgres.sh
 if [ -z "$TRAVIS" ]; then
-  mix coveralls.travis "$@"
+  mix coveralls.travis $*
 else
-  mix test "$@"
+  case "$*" in
+    *-h*)
+      mix help espec
+      exit 0
+      ;;
+    *--help*)
+      mix help espec
+      exit 0
+      ;;
+    *)
+      mix test "$*"
+      ;;
+  esac
 fi
 mix dialyzer
