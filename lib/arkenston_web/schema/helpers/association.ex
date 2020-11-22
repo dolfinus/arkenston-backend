63e776a192e6101e07b540f5515253a2932a1ab9
dolfinus <martinov_m_s_@mail.ru>
2020-11-22 19:19:31 +0300

    Add connect_with and connect_with_field macros to Association GraphQL helper

--- lib/arkenston_web/schema/helpers/association.ex
+++ lib/arkenston_web/schema/helpers/association.ex
@@ -21,4 +21,49 @@ defmodule ArkenstonWeb.Schema.Helpers.Association do
       end
     end
   end
+
+  defmacro connect_with(name, args \\ [])
+
+  defmacro connect_with(name, args) when is_list(args) do
+    non_null = args |> Keyword.get(:non_null, false)
+    if non_null do
+      do_connect_with(name, name, quote(do: non_null(unquote(name))))
+    else
+      do_connect_with(name, name, name)
+    end
+  end
+
+  defmacro connect_with(name, type) do
+    do_connect_with(name, name, type)
+  end
+
+  defmacro connect_with(name, target, type) do
+    do_connect_with(name, target, type)
+  end
+
+  def do_connect_with(name, target, type) do
+    quote do
+      field unquote(name), unquote(type) do
+        resolve assoc(unquote(target))
+      end
+    end
+  end
+
+  defmacro connect_with_field(target, field, type) do
+    do_connect_with_field(target, field, field, type)
+  end
+
+  defmacro connect_with_field(target, field, target_field, type) do
+    do_connect_with_field(target, field, target_field, type)
+  end
+
+  defp do_connect_with_field(target, field, target_field, type) do
+    quote do
+      field unquote(field), unquote(type) do
+        resolve assoc unquote(target), callback: fn result, _parent, _args ->
+          {:ok, result |> Map.get(unquote(target_field))}
+        end
+      end
+    end
+  end
 end
