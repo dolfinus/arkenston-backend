dc73390ed8b1f2fe96ba5180d164430605696905
dolfinus <martinov_m_s_@mail.ru>
2020-11-22 19:18:56 +0300

    Add TranslationHelper

---
+++ lib/arkenston/helper/translation_helper.ex
@@ -0,0 +1,229 @@
+defmodule Arkenston.Helper.TranslationHelper do
+  alias Arkenston.I18n
+  alias Trans.Translator
+
+  def nvl_field(value) do
+    case value do
+      nil ->
+        ""
+      value ->
+        value
+    end
+  end
+
+  defp atomize(input) when is_atom(input) do
+    input
+  end
+
+  defp atomize(input) do
+    input |> String.to_existing_atom()
+  end
+
+  def translate_field(object, field, locale \\ nil) do
+    case translation_container(object) do
+      value when map_size(value) > 0 ->
+        Translator.translate(object, field, locale || I18n.locale())
+      _ ->
+        nil
+    end |> nvl_field()
+  end
+
+  def get_struct(%{__struct__: struct}) do
+    struct
+  end
+
+  def get_struct(input) do
+    input
+  end
+
+  def translation_fields(module_or_struct) do
+    get_struct(module_or_struct).__trans__(:fields)
+  end
+
+  def translation_container_name(module_or_struct) do
+    get_struct(module_or_struct).__trans__(:container)
+  end
+
+  def translation_container(object) do
+    container_name = translation_container_name(object)
+    object |> Map.get(container_name) || %{}
+  end
+
+  def translation_locales(object) do
+    keys = translation_container(object) |> Map.keys() |> Enum.map(&String.to_atom/1)
+    all_locales = (keys ++ I18n.locales()) |> Enum.uniq()
+
+    all_locales
+  end
+
+  defp shrink_translation(translation) do
+    translation |> Enum.reduce(%{}, fn {key, value}, trans ->
+      key = atomize(key)
+      value = nvl_field(value)
+
+      unless byte_size(value) == 0 do
+        trans |> Map.put(key, value)
+      else
+        trans
+      end
+    end)
+  end
+
+  def nvl_translations_map(input) do
+    translations = translations_map(input)
+
+    translations |> Enum.reduce(%{}, fn {locale, old_translation}, acc ->
+      new_translation = old_translation |> shrink_translation()
+
+      unless Enum.empty?(new_translation) do
+        acc |> Map.put(locale, old_translation)
+      else
+        acc
+      end
+    end)
+  end
+
+  def shrink_translations_map(input) do
+    translations = translations_map(input)
+
+    new_translations = translations |> Enum.reduce(%{}, fn {locale, translation}, acc ->
+      translation = translation |> shrink_translation()
+
+      unless Enum.empty?(translation) do
+        acc |> Map.put(locale, translation)
+      else
+        acc
+      end
+    end)
+
+    if map_size(new_translations) == 0 do
+      nil
+    else
+      new_translations
+    end
+  end
+
+  def translations_map(input) do
+    input |> Enum.map(fn {locale, translation} ->
+      translation = translation |> Enum.map(fn {key, value} ->
+        key = atomize(key)
+        value = nvl_field(value)
+
+        {key, value}
+      end) |> Enum.into(%{})
+
+      {locale, translation}
+    end) |> Enum.into(%{})
+  end
+
+  def translations_from_object(object) do
+    fields = translation_fields(object)
+    locales = translation_locales(object)
+
+    locales |> Enum.map(fn locale ->
+      translation = fields |> Enum.map(fn field ->
+        field = atomize(field)
+        value = translate_field(object, field, locale)
+
+        {field, value}
+      end) |> Enum.into(%{})
+
+      {locale, translation}
+    end)|> Enum.into(%{})
+  end
+
+  def translations_list_from_object(object) do
+    translations_to_list(nvl_translations_map(translations_from_object(object)))
+  end
+
+  def translations_to_list(input) do
+    input |> Enum.reduce([], fn {locale, translation}, acc ->
+      translation = translation |> Enum.map(fn {key, value} ->
+          key = atomize(key)
+          value = nvl_field(value)
+
+          {key, value}
+        end)
+        |> Enum.into(%{})
+        |> Map.put(:locale, locale)
+
+      acc ++ [translation]
+    end)
+  end
+
+  def translations_from_list(input) do
+    input |> Enum.reduce(%{}, fn translation, acc ->
+      {locale, translation} = translation |> Map.pop(:locale, I18n.locale())
+      locale = atomize(locale)
+
+      translation = translation |> Enum.map(fn {key, value} ->
+          key = atomize(key)
+          value = nvl_field(value)
+
+          {key, value}
+        end)
+        |> Enum.into(%{})
+
+      acc |> Map.put(locale, translation)
+    end)
+  end
+
+  def merge_translations(input1, input2) do
+    input1 = translations_map(if is_list(input1) do
+      translations_from_list(input1)
+    else
+      input1
+    end)
+    input2 = translations_map(if is_list(input2) do
+      translations_from_list(input2)
+    else
+      input2
+    end)
+
+    Map.merge(input1, input2)
+  end
+
+  defmacro __using__(_opts \\ []) do
+    quote do
+      alias Arkenston.Helper.TranslationHelper
+
+      def create_translations(attrs) do
+        new_translations = attrs |> Map.get(:translations) || %{}
+
+        virtual_fields = TranslationHelper.translation_fields(__MODULE__)
+        virtual_fields_changes = attrs |> Map.take(virtual_fields)
+
+        attrs = attrs |> Map.drop(virtual_fields)
+
+        translations = TranslationHelper.merge_translations(new_translations, %{I18n.locale => virtual_fields_changes})
+
+        attrs
+        |> Map.drop(virtual_fields)
+        |> Map.put(:translations, TranslationHelper.shrink_translations_map(translations))
+      end
+
+      def update_translations(changeset, attrs) do
+        virtual_fields = TranslationHelper.translation_fields(__MODULE__)
+
+        translations = if Map.has_key?(attrs, :translations) do
+          new_translations = attrs |> Map.get(:translations) || %{}
+
+          virtual_fields_changes = attrs |> Map.take(virtual_fields)
+
+          TranslationHelper.merge_translations(new_translations, %{I18n.locale => virtual_fields_changes})
+        else
+          old_translations = changeset.translations || %{}
+
+          virtual_fields = TranslationHelper.translation_fields(__MODULE__)
+          virtual_fields_changes = attrs |> Map.take(virtual_fields)
+
+          TranslationHelper.merge_translations(old_translations, %{I18n.locale => virtual_fields_changes})
+        end
+
+        attrs
+        |> Map.drop(virtual_fields)
+        |> Map.put(:translations, TranslationHelper.shrink_translations_map(translations))
+      end
+    end
+  end
+end
