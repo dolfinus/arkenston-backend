a46f204bc8322aa29c1a2461b6baff5f01425a6f
dolfinus <martinov_m_s_@mail.ru>
2020-11-22 19:21:11 +0300

    Add custom Locale type into GraphQL schema

--- lib/arkenston_web/schema/types/custom.ex
+++ lib/arkenston_web/schema/types/custom.ex
@@ -2,6 +2,9 @@ defmodule ArkenstonWeb.Schema.Types.Custom do
   use Absinthe.Schema.Notation
 
   import_types ArkenstonWeb.Schema.Types.Custom.UUID4
+
+  import_types ArkenstonWeb.Schema.Types.Custom.Locale
+
   import_types ArkenstonWeb.Schema.Types.Custom.NonNegativeInteger
   import_types ArkenstonWeb.Schema.Types.Custom.PositiveInteger
   import_types ArkenstonWeb.Schema.Types.Custom.PageSize
---
+++ lib/arkenston_web/schema/types/custom/locale.ex
@@ -0,0 +1,37 @@
+defmodule ArkenstonWeb.Schema.Types.Custom.Locale do
+  use Absinthe.Schema.Notation
+
+  alias Arkenston.I18n
+
+  @desc "Locale name"
+  scalar :locale, name: "Locale" do
+    serialize(&encode/1)
+    parse(&decode/1)
+  end
+
+  @spec decode(Absinthe.Blueprint.Input.String.t()) :: {:ok, String.t} | :error
+  @spec decode(Absinthe.Blueprint.Input.Null.t()) :: {:ok, String.t}
+  defp decode(%Absinthe.Blueprint.Input.String{value: value}) do
+    try do
+      value = value |> String.to_existing_atom()
+
+      if I18n.locales() |> Enum.member?(value) do
+        {:ok, value |> to_string()}
+      else
+        :error
+      end
+    rescue
+      _ in ArgumentError -> :error
+    end
+  end
+
+  defp decode(%Absinthe.Blueprint.Input.Null{}) do
+    {:ok, I18n.locale}
+  end
+
+  defp decode(_) do
+    :error
+  end
+
+  defp encode(value), do: value |> to_string()
+end
