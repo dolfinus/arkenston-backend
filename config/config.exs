c0965c90131ba07b5f2fd7abcbc0e7c29dd49eaf
dolfinus <martinov_m_s_@mail.ru>
2020-11-23 23:12:03 +0300

    Allow to create email addresses with first domain name of any length

--- config/config.exs
+++ config/config.exs
@@ -22,7 +22,7 @@ config :arkenston,
     ],
     format: [
       name: ~r/^[a-zA-Z0-9_\-\.]+$/,
-      email: ~r/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/
+      email: ~r/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
     ]
   ]
 
