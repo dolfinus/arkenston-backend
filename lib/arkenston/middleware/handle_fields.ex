b05ebc596800e55c3d45dcf54af071e995c93999
dolfinus <martinov_m_s_@mail.ru>
2020-11-26 00:17:01 +0300

    Fix dialyzer errors

--- lib/arkenston/middleware/handle_fields.ex
+++ lib/arkenston/middleware/handle_fields.ex
@@ -3,22 +3,20 @@ defmodule Arkenston.Middleware.HandleFields do
 
   @behaviour Absinthe.Middleware
 
-  alias Absinthe.Resolution
-
-  @type resolution() :: Resolution.t()
+  @type resolution() :: Absinthe.Resolution.t()
   @type field() :: Absinthe.Blueprint.Document.Field.t() | Absinthe.Blueprint.Document.Fragment.Spread.t()
 
   @spec call(resolution(), any()) :: resolution()
   def call(%{context: context} = resolution, _config) do
     fields =
       resolution
-      |> Resolution.project()
+      |> Absinthe.Resolution.project()
       |> Enum.map(&handle_field/1)
 
     %{resolution | context: Map.put(context, :fields, fields)}
   end
 
-  @spec handle_field(field()) :: atom() | map()
+  @spec handle_field(field() | map()) :: atom() | {atom(), map() | list()}
   def handle_field(%{name: name, selections: selections}) when (is_list(selections) or is_map(selections)) and length(selections) == 0 do
     handle_field(%{name: name})
   end
@@ -27,11 +25,11 @@ defmodule Arkenston.Middleware.HandleFields do
     {handle_field(%{name: name}), Enum.map(selections, &handle_field/1)}
   end
 
-  def handle_field(%{name: name}) when is_binary(name) do
-    name |> String.to_atom()
+  def handle_field(%{name: name}) when is_atom(name) do
+    name
   end
 
   def handle_field(%{name: name}) do
-    name
+    :"#{name}"
   end
 end
