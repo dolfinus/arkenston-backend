117f679084c02be27884e0f6714ed4294c199492
dolfinus <martinov_m_s_@mail.ru>
2020-11-29 19:47:13 +0300

    Merge migrations

--- priv/repo/migrations/20201108220839_create_authors.exs
+++ priv/repo/migrations/20201108220839_create_authors.exs
@@ -5,12 +5,19 @@ defmodule Arkenston.Repo.Migrations.CreateAuthors do
   def up do
     create_audit_table :authors do
       add :name, :string, null: false
-      add :email, :string, null: false
+      add :email, :string, null: true
       add :translations, :map
     end
 
-    create unique_index(:authors_data, :name, where: "deleted IS FALSE")
-    create unique_index(:authors_data, :email, where: "deleted IS FALSE")
+    create unique_index(:authors_data, ["(lower(name))"],
+             name: :authors_data_name_index,
+             where: "deleted IS FALSE"
+           )
+
+    create unique_index(:authors_data, ["(lower(email))"],
+             name: :authors_data_email_index,
+             where: "deleted IS FALSE and email IS NOT NULL"
+           )
   end
 
   def down do
