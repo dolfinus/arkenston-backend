e400e8419a42d69f1e7c72d471953ff39e25d0b6
dolfinus <martinov_m_s_@mail.ru>
2020-11-29 20:07:23 +0300

    Fix dev entrypoint

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -5,10 +5,5 @@ set -e
 ./wait_for_postgres.sh
 mix ecto.setup
 
-if [[ "x$GENERATE_DOCS" == "xtrue" ]]; then
-  mix graphql.dump
-  mix graphql.docs
-fi
-
 mkdir -p ./_build && chown arkenston:arkenston -R ./_build
-mix phx.server "$*"
+mix phx.server $*
