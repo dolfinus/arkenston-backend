0608f6d5b2a6a75d2908e36c87e2e9112acc32e2
Maxim Martynov <martinov_m_s_@mail.ru>
2023-08-06 18:03:00 +0300

    Fix Trans usage

--- lib/arkenston/subject/author.ex
+++ lib/arkenston/subject/author.ex
@@ -8,7 +8,7 @@ defmodule Arkenston.Subject.Author do
 
   alias Arkenston.Subject.User
 
-  use Trans, translates: [:first_name, :middle_name, :last_name], default_locale: :en
+  use Trans, translates: [:first_name, :middle_name, :last_name], default_locale: "EN"
 
   @type lang :: atom :: String.t()
 
